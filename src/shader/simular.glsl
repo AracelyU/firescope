c0638834298ad4c5a49e4d1fa921ea640a17f8f1
SpiderKnightAcademico <john.fernandez.c@usach.cl>
2025-12-19 19:59:54 -0300

    add: Simulación básica usando GPU

---
+++ src/shader/simular.glsl
@@ -0,0 +1,36 @@
+#version 430
+
+layout(local_size_x = 32, local_size_y = 32) in;
+
+layout(std430, binding = 0) buffer EstadoEntrada {
+    int entrada[];
+};
+layout(std430, binding = 1) buffer EstadoSalida {
+    int salida[];
+};
+
+int indice_en(int x, int y){
+    return x + 32 * y;
+}
+int estado_en(int x, int y){
+    if(x < 0 || x > 31 || y < 0 || y > 31) return -1;
+    return entrada[indice_en(x, y)];
+}
+
+void main(){
+    int x = int(gl_GlobalInvocationID.x);
+    int y = int(gl_GlobalInvocationID.y);
+    int i = indice_en(x, y);
+    int siguiente = entrada[i];
+
+    if(entrada[i] == 1){
+        siguiente = 2;
+    }
+    else if(entrada[i] == 0){
+        if((estado_en(x + 1, y) == 1) || (estado_en(x - 1, y) == 1) || (estado_en(x, y + 1) == 1) || (estado_en(x, y - 1) == 1)){
+            siguiente = 1;
+        }
+    }
+
+    salida[i] = siguiente;
+}
